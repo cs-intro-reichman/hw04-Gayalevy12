2dd8529769380eef1e8333715accbce7222ba62f
Gaya Levy <gayalevy@Mac.lan>
2025-11-27 17:27:19 +0200

    completed assignment

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -189,20 +189,28 @@ public class ArrCharOps {
     {
     if(str1 == null || str2 == null) 
     {
-        return -2;
+        return -2; 
     }
     int len1 = str1.length();
     int len2 = str2.length();
-    int minLen = Math.min(len1, len2);    
+    int minLen = Math.min(len1, len2); 
     for(int i = 0; i < minLen; i++)
     {
         char char1 = str1.charAt(i);
         char char2 = str2.charAt(i);
-        if(char1 != char2)
-        {
-            return char1 - char2; 
+        if (char1 < char2) {
+            return -1;
+        }
+        if (char1 > char2) {
+            return 1;
         }
     }
-    return len1 - len2;
+    if (len1 == len2) {
+        return 0;
+    }
+    if (len1 < len2) {
+        return -1;
+    }
+    return 1;
 }
 }
